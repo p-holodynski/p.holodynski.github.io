57d3da1f68b38c428ddbc317eb29d4638c6bcdfb
phjoker <phjok3r@gmail.com>
2017-03-14 18:25:13 +0000

    Major update

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#accordion").accordion({
+        collapsible: true,
+        active: false,
+        heightStyle: "content",
+        //event: "mouseover",
+    });
+    //.on('mouseleave', function() {
+    //$(this).accordion("option", "active", false);
+    //});
+
+});
